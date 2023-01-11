514d7ec115b0effe740f4993b0569bad1fff7e26
sleeptightwakebright@hotmail.com <sleeptightwakebright@hotmail.com>
2023-01-11 12:22:59 +1100

    server is up and register page made

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 1001;
+const PORT = process.env.PORT || 1001; 
+
+//allows express to render ejs views
+app.set('view-engine', 'ejs');
+//allows the use of static files in views like my style.css etc.
+app.use('/views', express.static('views')); 
+
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs');
+}); 
+
+app.get('/register', (req, res) => {
+    res.render('register.ejs');
+});
 
 app.listen(PORT, () => {
     console.log('The server for complaing your life away has begun..');
